65282fb3a4242cbf794af25efcc7d1b0b2e00017
Mojahidul Islam <78002811+Nightstalkeer@users.noreply.github.com>
2022-10-15 15:35:03 +0000

    Meals completed

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,9 +1,36 @@
 import { useGlobalContext } from '../context';
+import { BsHandThumbsUp } from 'react-icons/bs';
 
 const Meals = () => {
-  const context = useGlobalContext();
-  console.log(context);
-  return <h1>Meals components</h1>
+  const { meals, loading } = useGlobalContext();
+
+  if (loading) {
+    return <section className="section">
+      <h4>Loading...</h4>
+    </section>
+  }
+  if (meals.length < 1) {
+    return <section className="section">
+      <h4>No meals matched your search term. Please try again.</h4>
+    </section>
+  }
+
+  // console.log(context);
+  return <section className="section-center">
+    {meals.map((singleMeal) => {
+      const { idMeal, strMeal: title, strMealThumb: image } = singleMeal
+      // console.log(singleMeal);     
+
+
+      return <article key={idMeal} className="single-meal">
+        <img src={image} className="img" />
+        <footer>
+          <h5>{title}</h5>
+          <button className="like-btn"><BsHandThumbsUp /></button>
+        </footer>
+      </article>
+    })}
+  </section>
 }
 
 export default Meals;
--- src/context.jsx
+++ src/context.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import axios from 'axios';
 
 const AppContext = React.createContext();
@@ -9,6 +9,8 @@ const randomMealUrl = 'https://www.themealdb.com/api/json/v1/1/random.php';
 
 
 const AppProvider = ({ children }) => {
+  const [loading, setLoading] = useState(false);
+  const [meals, setMeals] = useState([]);
 
   /* Below method used fetch API */
 
@@ -30,20 +32,28 @@ const AppProvider = ({ children }) => {
   /* Below method used AXIOS package */
 
   const fetchMeals = async (url) => {
+    setLoading(true);
     try {
-      const response = await axios(url);
-      console.log(response);
+      const { data } = await axios(url);
+      if (data.meals) {
+        setMeals(data.meals);
+      }
+      else {
+        setMeals([]);
+      }
+      // console.log(data);
     } catch (error) {
-      console.log(error);
+      console.log(error.response);
     }
+    setLoading(false);
   }
 
   useEffect(() => {
     fetchMeals(allMealsUrl);
-  }, [])
+  }, []);
 
 
-  return <AppContext.Provider value={{ name: 'john', role: 'student' }} >
+  return <AppContext.Provider value={{ loading, meals }} >
     {children}
   </AppContext.Provider>
 }
